145bac79c0d385b742c9b745d66da8602bdc9f4f
Kevin <kevin@habithq.ca>
2024-02-07 12:15:22 -0700

    Add project structure, begin layout.tsx and page.tsx for homepage.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        BCSans: ["BCSans", "sans-serif"],
+      },
+      colors: {
+        "primary-bc-white": "#f1f1f1",
+        "primary-bc-blue": "#355992",
+        "preview-bg-green": "#00a651"
       },
     },
   },
